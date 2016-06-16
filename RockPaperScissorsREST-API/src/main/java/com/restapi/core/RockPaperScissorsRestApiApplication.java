a2d4f5bce44691f817d31157ce7c12009b2f4fa0
ozzman02 <osantamaria@gmail.com>
2016-06-16 10:51:41 -0600

    Creating project structure

---
+++ RockPaperScissorsREST-API/src/main/java/com/restapi/core/RockPaperScissorsRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.restapi.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RockPaperScissorsRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RockPaperScissorsRestApiApplication.class, args);
+	}
+}
