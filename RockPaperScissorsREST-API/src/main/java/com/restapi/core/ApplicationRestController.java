987d56fb685e3ddaf09c39578424ae75ea54724e
ozzman02 <osantamaria@gmail.com>
2016-06-16 23:41:18 -0600

    Resolve tournament complete

--- RockPaperScissorsREST-API/src/main/java/com/restapi/core/ApplicationRestController.java
+++ RockPaperScissorsREST-API/src/main/java/com/restapi/core/ApplicationRestController.java
@@ -18,8 +18,9 @@ public class ApplicationRestController {
 			return new ResponseEntity<Error>(new Error("The number of participantes must be an even number"), 
 					HttpStatus.FORBIDDEN);
 		} else if (ApplicationUtils.validMatch(games)) { 
-			Game game = ApplicationUtils.resolveMatch(games);
-			return new ResponseEntity<Game>(game, HttpStatus.OK);
+			ArrayList<Game> results = ApplicationUtils.resolveMatch(games);
+			Game winner = results.get(0);
+			return new ResponseEntity<Game>(winner, HttpStatus.OK);
 		} else {
 			return new ResponseEntity<Error>(new Error("There are duplicate player names"), 
 					HttpStatus.FORBIDDEN);
@@ -44,11 +45,14 @@ public class ApplicationRestController {
 	
 	@RequestMapping(value = "/rockpaperscissors/getTournament", method = RequestMethod.GET)
 	public ResponseEntity<Tournament> getTournament() {
+		
 		Game g1 = new Game("Oscar", "S");
 		Game g2 = new Game("Allen", "P");
 		Game g3 = new Game("Mike", "R");
 		Game g4 = new Game("Luis", "S");
+		
 		ArrayList<Game> games = new ArrayList<Game>();
+		
 		games.add(g1);
 		games.add(g2);
 		games.add(g3);
@@ -59,41 +63,34 @@ public class ApplicationRestController {
 		return new ResponseEntity<Tournament>(tournament, HttpStatus.OK);
 	}
 	
-	@RequestMapping(value = "/rockpaperscissors/getTournaments", method = RequestMethod.GET)
-	public ResponseEntity<List<Tournament>> getTournaments() {
+	@RequestMapping(value = "/rockpaperscissors/tournament", method = RequestMethod.GET)
+	public ResponseEntity<Tournament> checkTournament() {
 		
 		Game g1 = new Game("Oscar", "S");
 		Game g2 = new Game("Allen", "P");
 		Game g3 = new Game("Mike", "R");
 		Game g4 = new Game("Luis", "S");
-		
 		Game g5 = new Game("Mario", "S");
 		Game g6 = new Game("Carlos", "P");
 		Game g7 = new Game("Juana", "R");
 		Game g8 = new Game("Laura", "S");
 		
-		ArrayList<Game> games1 = new ArrayList<Game>();
-		ArrayList<Game> games2 = new ArrayList<Game>();
+		ArrayList<Game> games = new ArrayList<Game>();
+	
+		games.add(g1);
+		games.add(g2);
+		games.add(g3);
+		games.add(g4);		
+		games.add(g5);
+		games.add(g6);
+		games.add(g7);
+		games.add(g8);
 		
-		games1.add(g1);
-		games1.add(g2);
-		games1.add(g3);
-		games1.add(g4);
+		Tournament tournament = new Tournament(games);
 		
-		games1.add(g5);
-		games1.add(g6);
-		games1.add(g7);
-		games1.add(g8);
+		ApplicationUtils.resolveTournament(tournament.getGames());
 		
-		Tournament tournament1 = new Tournament(games1);
-		Tournament tournament2 = new Tournament(games2);
-		
-		ArrayList<Tournament> tournaments = new ArrayList<Tournament>();
-		
-		tournaments.add(tournament1);
-		tournaments.add(tournament2);
-		
-		return new ResponseEntity<List<Tournament>>(tournaments, HttpStatus.OK);
+		return new ResponseEntity<Tournament>(tournament, HttpStatus.OK);
 	}
 	
 	
--- RockPaperScissorsREST-API/src/main/java/com/restapi/core/ApplicationUtils.java
+++ RockPaperScissorsREST-API/src/main/java/com/restapi/core/ApplicationUtils.java
@@ -11,14 +11,15 @@ import java.util.Set;
 public class ApplicationUtils {
 	
 	static final Map<String, String> table = new HashMap<String, String>();
-	static List<Game> tournamentGames = new ArrayList<Game>();
 	static ArrayList<Game> match = new ArrayList<Game>();
 	static ArrayList<Game> winners = new ArrayList<Game>();
 	
-	public static Game resolveMatch(List<Game> games) {
+	public static ArrayList<Game> resolveMatch(List<Game> games) {
 		
 		Game g1 = games.get(0);
 		Game g2 = games.get(1);
+		Game firstPlace = new Game();
+		Game secondPlace = new Game();
 		
 		table.put("P,R", g1.getPlayerName() + "," + g1.getStrategy());
 		table.put("P,S", g2.getPlayerName() + "," + g2.getStrategy());
@@ -36,30 +37,55 @@ public class ApplicationUtils {
 		String key = player1Strategy + "," + player2Strategy;
 		
 		if(table.containsKey(key)) {
+			
 			String[] winnerData = table.get(key).split(",");
-			Game winnerGame = new Game(winnerData[0].trim(), winnerData[1].trim());
-			return winnerGame;
+			firstPlace.setPlayerName(winnerData[0].trim());
+			firstPlace.setStrategy(winnerData[1].trim());
+			
+			if (winnerData[0].trim().equals(g1.getPlayerName())) {
+				secondPlace.setPlayerName(g2.getPlayerName());
+				secondPlace.setStrategy(g2.getStrategy());
+			} else if (winnerData[0].trim().equals(g2.getPlayerName())) {
+				secondPlace.setPlayerName(g1.getPlayerName());
+				secondPlace.setStrategy(g1.getStrategy());
+			}
+			
+			ArrayList<Game> results = new ArrayList<Game>();
+			results.add(firstPlace);
+			results.add(secondPlace);
+			
+			return results;
+			
 		} else {
 			return null;
 		}
 	}
 	
-	public static Game resolveTournament(ArrayList<Tournament> tournaments) {
+	public static ArrayList<Game> resolveTournament(List<Game> games) {
 		int count = 0;
-		for (Tournament tournament : tournaments) {
-			ArrayList<Game> games = tournament.getGames();
-			for(Game game : games) {
-				match.add(game);
-				count++;
-				if (count % 2 == 0) {
-					Game winner = resolveMatch(match);
-					winners.add(winner);
-					count = 0;
-				}
+		for (Game game : games) {
+			match.add(game);
+			count++;
+			if (count % 2 == 0) {
+				winners.add(resolveMatch(match).get(0));
+				match.remove(0);
+				match.remove(0);
+				count = 0;
 			}
 		}
-		return null;
+		ArrayList<Game> list = new ArrayList<Game>();
+		for(Game g : winners) {
+			Game data = new Game(g.getPlayerName(), g.getStrategy());
+			list.add(data);
+		}
+		winners.clear();
+		if(list.size() == 2) {
+			winners = resolveMatch(list);
+		} else {
+			resolveTournament(list);
+		}	
 		
+		return winners;
 	}
 	
 	private static boolean duplicatesFound(List<String> players) {
